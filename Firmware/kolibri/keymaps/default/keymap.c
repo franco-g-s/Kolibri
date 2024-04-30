7f89a1b7fcaf5f21581df6d78b1a934e9fe13f16
franco-g-s <106225458+franco-g-s@users.noreply.github.com>
2024-04-30 18:22:25 +0200

    Moved modifier keys around
    added layer key as M5

--- Firmware/kolibri/keymaps/default/keymap.c
+++ Firmware/kolibri/keymaps/default/keymap.c
@@ -19,16 +19,16 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
-        KC_PGUP, KC_2,    KC_ESC,  KC_1,    KC_MINS, KC_EQL,  KC_BSPC, KC_PGDN, KC_TAB,  KC_Q,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL,  KC_CAPS, KC_A,    KC_SCLN, KC_QUOT, KC_ENT,  KC_MUTE, KC_LSFT, KC_Z,    KC_DOT,  KC_SLSH, KC_LSFT, KC_UP,
-        KC_LCTL, KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT, KC_4,    KC_5,    KC_6,    KC_3,
+        KC_PGUP, KC_2,    KC_ESC,  KC_1,    KC_MINS, KC_EQL,  KC_BSPC, KC_PGDN, KC_TAB,  KC_Q,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, MO(1),  KC_CAPS, KC_A,    KC_SCLN, KC_QUOT, KC_ENT,  KC_MUTE, KC_LSFT, KC_Z,    KC_DOT,  KC_SLSH, KC_LSFT, KC_UP,
+        KC_LCTL, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT, KC_4,    KC_5,    KC_6,    KC_3,
         KC_W,    KC_E,    KC_R,    KC_T,
         KC_S,    KC_D,    KC_F,    KC_G,
         KC_X,    KC_C,    KC_V,    KC_B,
-        KC_LALT, KC_SPC,  MO(1),   KC_7,    KC_8,    KC_9,    KC_0,
+        KC_LALT, KC_SPC,  KC_LCMD,   KC_7,    KC_8,    KC_9,    KC_0,
         KC_Y,    KC_U,    KC_I,    KC_O,
         KC_H,    KC_J,    KC_K,    KC_L,
         KC_B,    KC_N,    KC_M,    KC_COMM,
-        KC_SPC,  KC_LALT
+        KC_SPC,  KC_RCMD
     ),
 
     [1] = LAYOUT(
